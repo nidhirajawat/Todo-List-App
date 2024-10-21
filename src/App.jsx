07ed692e357b232901e9a1441fb1763dc2f3acde
Nikita Rajawat <nikita@gmail.com>
2024-10-21 23:26:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todos from './Components/Todos';
+
+function App() {
+  return (
+    <>
+    welcome
+    <Todos/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Todos.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+
+const Todos = () => {
+    // States
+    const [initial, setInitial] = useState();
+    const [data, setData] = useState([]);
+
+    const getInput = (e)=>{
+        console.log(e.target.value);
+        setInitial(e.target.value)
+    }
+    const getData = () =>{
+        console.log(initial);
+        let store = [...data, initial]
+        setData(store)
+        setInitial("")
+    }
+    const deleteTask= (index) =>{
+      console.log(index);
+      let filterData = data.filter((curElem, id)=>{
+        return id !== index;
+      })
+      setData(filterData)
+    }
+    console.log(data);
+  return (
+    <>
+      <div className="container">
+        <div className="inputTask">
+            <input type="text" placeholder='Enter Your Task' value={initial} onChange={getInput} />
+            <button onClick={getData}>Add</button>
+        </div>
+        {data.map((curVal, index)=>{
+            return(
+                <>
+                    <div className="taskData">
+                      <p>{curVal}</p>
+                      <i id='deleteIcon' onClick={()=>deleteTask(index)} class="fa-solid fa-trash"></i>
+                    </div>
+                </>
+            )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Todos
